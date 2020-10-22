ff51212d5334f20c9fecafea20630819a03929f5
Yannig-IIALaval <55286649+Yannig-IIALaval@users.noreply.github.com>
2020-10-22 21:01:42 +0200

    Admin final

--- src/main/java/com/github/khornya/useyourwords/controller/HomeController.java
+++ src/main/java/com/github/khornya/useyourwords/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.github.khornya.useyourwords.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-    @GetMapping
-    public String home() {
-        return "home";
+
+    @GetMapping("")
+    public String add() {
+        return "home/home";
     }
 }
