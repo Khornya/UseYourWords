72efc49d8aa8d51fc025f2c02958a53bc146f608
Khornya <marine.khornya@gmail.com>
2020-10-31 00:02:29 +0100

    import cleanup

--- src/main/java/com/github/khornya/useyourwords/config/TomcatConfig.java
+++ src/main/java/com/github/khornya/useyourwords/config/TomcatConfig.java
@@ -1,8 +1,6 @@
 package com.github.khornya.useyourwords.config;
 
-import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.AbstractHttp11Protocol;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
