5f6758cd4988623405b54f2736e68c84a3a9f4d6
Khornya <marine.khornya@gmail.com>
2020-10-30 23:52:03 +0100

    added response status

--- src/main/java/com/github/khornya/useyourwords/exception/ScoreNotFoundException.java
+++ src/main/java/com/github/khornya/useyourwords/exception/ScoreNotFoundException.java
@@ -1,5 +1,9 @@
 package com.github.khornya.useyourwords.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Score")
 public class ScoreNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 }
